d0d3e03451febd957f0d6d20ca29edad557353ff
SoKyungHyun <valentine92exp@gmail.com>
2024-06-19 17:03:24 +0900

    move db config to database module

--- backend/database/database.go
+++ backend/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"log"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,10 +12,14 @@ var (
 	DBConn *gorm.DB
 )
 
-func InitDB(config *Config) error {
+func InitDB() error {
+	config, err := LoadConfig("database/config.json")
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		config.DBUser, config.DBPass, config.DBHost, config.DBPort, config.DBName)
-	var err error
 	DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return fmt.Errorf("failed to connect to database: %w", err)
--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"log"
-
 	"cywell.com/vacation-promotion/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	config, err := database.LoadConfig("database/config.json")
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
 
 	app := fiber.New()
-
-	database.InitDB(config)
+	database.InitDB()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
