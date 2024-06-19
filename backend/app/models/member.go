670d2fb42535e399d7545233855917f557dd95cc
SoKyungHyun <valentine92exp@gmail.com>
2024-06-19 21:59:38 +0900

    migration code

--- backend/app/models/member.go
+++ backend/app/models/member.go
@@ -3,18 +3,18 @@ package models
 import "time"
 
 type Member struct {
-	ID             uint    `gorm:"primaryKey"`
-	CompanyID      uint    `gorm:"index"`
-	Company        Company `gorm:"foreignKey:CompanyID"`
-	Name           string  `gorm:"size:100"`
-	Email          string  `gorm:"size:100;unique"`
-	Password       string  `gorm:"size:100"`
-	HireDate       time.Time
-	RetireDate     *time.Time
-	IsActive       bool
-	Admin          []*Company            `gorm:"many2many:member_admin"`
-	GivenVacations []GivenVacation       `gorm:"foreignKey:MemberID"`
-	ApplyVacations []ApplyVacation       `gorm:"foreignKey:MemberID"`
-	Groups         []*Group              `gorm:"many2many:group_members"`
-	Notifications  []*NotificationMember `gorm:"foreignKey:MemberID"`
+	ID                  uint    `gorm:"primaryKey"`
+	CompanyID           uint    `gorm:"index"`
+	Company             Company `gorm:"foreignKey:CompanyID"`
+	Name                string  `gorm:"size:100"`
+	Email               string  `gorm:"size:100;unique"`
+	Password            string  `gorm:"size:100"`
+	HireDate            time.Time
+	RetireDate          *time.Time
+	IsActive            bool
+	Admin               []*Company            `gorm:"many2many:member_admin"`
+	GivenVacations      []GivenVacation       `gorm:"foreignKey:MemberID"`
+	ApplyVacations      []ApplyVacation       `gorm:"foreignKey:MemberID"`
+	Groups              []*Group              `gorm:"many2many:group_members"`
+	NotificationMembers []*NotificationMember `gorm:"foreignKey:MemberID"`
 }
--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cywell.com/vacation-promotion/app/models"
 	"cywell.com/vacation-promotion/database"
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,6 +10,17 @@ func main() {
 
 	app := fiber.New()
 	database.InitDB()
+	database.DBConn.AutoMigrate(
+		&models.Company{},
+		&models.Member{},
+		&models.MemberAdmin{},
+		&models.NotificationMember{},
+		&models.Group{},
+		&models.GivenVacation{},
+		&models.ApplyVacation{},
+		&models.VacationPlan{},
+		&models.Notification{},
+	)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
