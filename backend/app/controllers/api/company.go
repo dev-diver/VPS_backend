0245acfeee0bbc6769c416b256947531df6f32c7
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-06-30 23:56:33 +0900

    make createCompany func to reuse

--- backend/app/controllers/api/company.go
+++ backend/app/controllers/api/company.go
@@ -1,56 +1,57 @@
 package api
 
 import (
-	"log"
+	"errors"
 
-	"cywell.com/vacation-promotion/app/auth"
 	"cywell.com/vacation-promotion/app/dto"
 	"cywell.com/vacation-promotion/app/models"
 	"cywell.com/vacation-promotion/database"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
+
+func CreateCompany(tx *gorm.DB, company models.Company) (*models.Company, *models.Organize, error) {
+
+	if tx.Error != nil {
+		return nil, nil, errors.New("could not begin transaction")
+	}
+
+	// 회사 생성
+	if err := tx.Create(&company).Error; err != nil {
+		tx.Rollback()
+		return nil, nil, errors.New("could not create company")
+	}
+
+	// 조직 생성
+	organize := models.Organize{
+		CompanyID: company.ID,
+		Name:      company.Name,
+		ParentID:  nil,
+	}
+
+	if err := tx.Create(&organize).Error; err != nil {
+		tx.Rollback()
+		return nil, nil, errors.New("could not create organize")
+	}
+
+	return &company, &organize, nil
+}
 
 func CreateCompanyHandler(db *database.Database) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-
-		_, err := auth.CheckToken(c)
-		if err != nil {
-			log.Println(err)
-			return c.SendStatus(401)
-		}
 
 		company := new(models.Company)
 		if err := c.BodyParser(company); err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 		}
 
-		// 트랜잭션 시작
 		tx := db.DB.Begin()
-		if tx.Error != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "could not start transaction"})
-		}
-
-		// 회사 생성
-		if err := tx.Create(&company).Error; err != nil {
-			tx.Rollback()
+		company, _, err := CreateCompany(tx, *company)
+		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 		}
-
-		// 조직 생성
-		organize := models.Organize{
-			CompanyID: company.ID,
-			Name:      company.Name,
-			ParentID:  nil,
-		}
-
-		if err := tx.Create(&organize).Error; err != nil {
-			tx.Rollback()
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-		}
-
-		// 트랜잭션 커밋
 		if err := tx.Commit().Error; err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "could not commit transaction"})
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 		}
 
 		return c.Status(fiber.StatusCreated).JSON(company)
