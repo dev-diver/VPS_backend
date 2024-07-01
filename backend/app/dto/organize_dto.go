cea631e9a50afcfbdd5d3022ed9184a9093c871f
SoKyungHyun <valentine92exp@gmail.com>
2024-07-01 09:46:17 +0900

    make Organize Response DTO

--- backend/app/dto/organize_dto.go
+++ backend/app/dto/organize_dto.go
@@ -1,5 +1,25 @@
 package dto
 
+import "cywell.com/vacation-promotion/app/models"
+
 type OrganizeRequest struct {
 	Name string `json:"name" validate:"required"`
+}
+
+type OrganizeResponse struct {
+	ID       uint               `json:"organize_id"`
+	Name     string             `json:"organize_name"`
+	ParentID *uint              `json:"parent_id"`
+	Members  []MemberResponse   `json:"members,omitempty"`
+	Children []OrganizeResponse `json:"children,omitempty"`
+}
+
+func MapOrganizeToResponse(organize models.Organize) OrganizeResponse {
+
+	return OrganizeResponse{
+		ID:       organize.ID,
+		Name:     organize.Name,
+		ParentID: organize.ParentID,
+		Members:  MapMembersToDTO(organize.Members),
+	}
 }
