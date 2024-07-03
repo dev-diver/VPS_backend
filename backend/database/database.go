2d25a7af797d421498d10100a9c9afb03bd0bc80
SoKyungHyun <valentine92exp@gmail.com>
2024-07-03 13:24:24 +0900

    config with env file

--- backend/database/database.go
+++ backend/database/database.go
@@ -3,7 +3,9 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,15 +15,22 @@ type Database struct {
 }
 
 func InitDB() (*Database, error) {
-	var db *gorm.DB
-	config, err := LoadConfig("./database/config.json")
+
+	err := godotenv.Load("./database/.env")
 	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
+		log.Fatalf("Error loading .env file")
 	}
 
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbName := os.Getenv("DATABASE_NAME")
+	fmt.Println("dbUser:", dbUser, "dbPassword:", dbPassword, "dbHost:", dbHost, "dbPort:", dbPort, "dbName:", dbName)
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.DBUser, config.DBPass, config.DBHost, config.DBPort, config.DBName)
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
