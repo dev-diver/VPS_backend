8836eb1d14530aca241065d3c7f3dc794bb4f0b9
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-07 20:05:17 +0900

    .env error log

--- backend/database/database.go
+++ backend/database/database.go
@@ -18,7 +18,7 @@ func InitDB() (*Database, error) {
 
 	err := godotenv.Load("./database/.env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file: %v", err)
 	}
 
 	dbUser := os.Getenv("DATABASE_USER")
