f431acd8730a49a8d501c801da039d8a6c2025c6
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-07-08 23:22:41 +0900

    split response

--- backend/app/controllers/api/webhook.go
+++ backend/app/controllers/api/webhook.go
@@ -15,6 +15,9 @@ type WebhookData struct {
 		Tag       string `json:"tag"`
 		MediaType string `json:"media_type"`
 	} `json:"push_data"`
+	Repository struct {
+		RepoName string `json:"repo_name"`
+	} `json:"repository"`
 }
 
 func WebhookHandler() fiber.Handler {
@@ -27,41 +30,84 @@ func WebhookHandler() fiber.Handler {
 
 		log.Printf("Webhook received: %+v", webhookData)
 
-		//client stop
-		if err := dockerRequest("POST", "/containers/client/stop", nil); err != nil {
-			log.Printf("Failed to stop container: %v", err)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
+		if imageName := webhookData.Repository.RepoName; imageName == "devdiver/vacation_promotion_client" {
+			if err := clientRestart(imageName); err != nil {
+				return err
+			}
+		} else if imageName == "devdiver/vacation_promotion_server" {
+			if err := serverRestart(imageName); err != nil {
+				return err
+			}
+		} else {
+			log.Printf("Unknown repository: %s", webhookData.Repository.RepoName)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid repository"})
 		}
-
-		//client rm -f
-		if err := dockerRequest("DELETE", "/containers/client", nil); err != nil {
-			log.Printf("Failed to remove container: %v", err)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
-		}
-
-		//compose pull client
-		imageName := "client:latest"
-		pullImageUrl := fmt.Sprintf("/images/create?fromImage=%s", imageName)
-		if err := dockerRequest("POST", pullImageUrl, nil); err != nil {
-			log.Printf("Failed to pull images: %v", nil)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
-		}
-
-		//compose run -d client
-		runContainerData := []byte(fmt.Sprintf(`{"Image": "%s"}`, imageName))
-		if err := dockerRequest("POST", "/containers/create", runContainerData); err != nil {
-			log.Printf("Failed to create container: %v", err)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
-		}
-
-		// //docker restart server
-		// if err := dockerRequest("POST", "/containers/server/restart", nil); err != nil {
-		// 	log.Printf("Failed to restart container: %v", err)
-		// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal server error"})
-		// }
 
 		return c.JSON(fiber.Map{"status": "Success"})
 	}
+}
+
+func clientRestart(imageName string) error {
+	//client stop
+	if err := dockerRequest("POST", "/containers/client/stop", nil); err != nil {
+		log.Printf("Failed to stop client container: %v", err)
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+
+	//client rm -f
+	if err := dockerRequest("DELETE", "/containers/client", nil); err != nil {
+		log.Printf("Failed to remove client container: %v", err)
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+
+	//compose pull client
+	if err := imagePull(imageName); err != nil {
+		return err
+	}
+
+	//compose run -d client
+	runContainerData := []byte(`{
+		"Image": "devdiver/vacation_promotion_client:latest",
+		"HostConfig": {
+			"Binds": ["/front_app:/dist"],
+			"Command": ["sh", "-c", "rm -rf /dist/* && mv /app/front_web/dist/* /dist && bin/true"]
+		}
+	}`)
+	if err := dockerRequest("POST", "/containers/create", runContainerData); err != nil {
+		log.Printf("Failed to create container: %v", err)
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+
+	if err := dockerRequest("POST", "/containers/vacation_promotion_client/start", nil); err != nil {
+		log.Printf("Failed to start client container: %v", err)
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+
+	return nil
+}
+
+func serverRestart(imageName string) error {
+
+	//image pull server
+	if err := imagePull(imageName); err != nil {
+		return err
+	}
+
+	//docker restart server
+	if err := dockerRequest("POST", "/containers/server/restart", nil); err != nil {
+		log.Printf("Failed to restart container: %v", err)
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+	return nil
+}
+
+func imagePull(imageName string) error {
+	pullImageUrl := fmt.Sprintf("/images/create?fromImage=%s:latest", imageName)
+	if err := dockerRequest("POST", pullImageUrl, nil); err != nil {
+		log.Printf("Failed to pull images: %v", nil)
+		return fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+	}
+	return nil
 }
 
 func dockerRequest(method, command string, jsonData []byte) error {
@@ -71,10 +117,10 @@ func dockerRequest(method, command string, jsonData []byte) error {
 	var req *http.Request
 	var err error
 	if jsonData != nil {
-		req, err = http.NewRequest(method, url, bytes.NewBuffer(jsonData))
+		req, err = http.NewRequest(method, url+command, bytes.NewBuffer(jsonData))
 		req.Header.Set("Content-Type", "application/json")
 	} else {
-		req, err = http.NewRequest(method, url, nil)
+		req, err = http.NewRequest(method, url+command, nil)
 	}
 
 	if err != nil {
